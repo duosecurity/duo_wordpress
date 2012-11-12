a79349ee8abdfc0b1bdb83ca9f9c87a890aa35a0
David Wilemski <david@davidwilemski.com>
2012-11-12 14:09:10 -0500

    Duo Wordpress 1.4
    Includes:
    - WordPress multisite support
    - now uses wp_login_url()
    - verified WordPress 3.5 support

--- duo_wordpress.php
+++ duo_wordpress.php
@@ -3,7 +3,7 @@
 Plugin Name: Duo Two-Factor Authentication
 Plugin URI: https://github.com/duosecurity/duo_wordpress
 Description: This plugin enables Duo two-factor authentication for WordPress logins.
-Version: 1.3.4
+Version: 1.4
 Author: Duo Security
 Author URI: http://www.duosecurity.com
 License: GPL2
@@ -29,9 +29,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     require_once("duo_web/duo_web.php");
 
     function duo_sign_request($user, $redirect) {
-        $ikey = get_option('duo_ikey');
-        $skey = get_option('duo_skey');
-        $host = get_option('duo_host');
+        $ikey = duo_get_option('duo_ikey');
+        $skey = duo_get_option('duo_skey');
+        $host = duo_get_option('duo_host');
        
         $username = $user->user_login;
 
@@ -71,7 +71,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
             <script>
             Duo.init({
                 'host': <?php echo "'" . $host . "'"; ?>,
-                'post_action':'wp-login.php',
+                'post_action': '<?php echo wp_login_url() ?>',
                 'sig_request':<?php echo "'" . $request_sig . "'"; ?>
             });
             </script>
@@ -99,7 +99,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
         if ( defined('XMLRPC_REQUEST') && XMLRPC_REQUEST ) 
             return; //allows the XML-RPC protocol for remote publishing
 
-        if (get_option("duo_ikey", "") == "" || get_option("duo_skey", "") == "" || get_option("duo_host", "") == "") {
+        if (duo_get_option("duo_ikey", "") == "" || duo_get_option("duo_skey", "") == "" || duo_get_option("duo_host", "") == "") {
             return;
         }
 
@@ -111,7 +111,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
             if (wp_hash($_POST['uhash']) == wp_hash($sig) && time() < $expire) {
                 $user = get_userdatabylogin($_POST['u']);
 
-                if ($user->user_login == Duo::verifyResponse(get_option('duo_skey'), $_POST['sig_response'])) {
+                if ($user->user_login == Duo::verifyResponse(duo_get_option('duo_skey'), $_POST['sig_response'])) {
                     wp_set_auth_cookie($user->ID);
                     wp_safe_redirect($_POST['redirect_to']);
                     exit();
@@ -136,8 +136,19 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 				$all_roles[strtolower(before_last_bar($r))] = ucfirst(before_last_bar($r));
 			}
 
-            $duo_roles = get_option('duo_roles', $all_roles); 
+            $duo_roles = duo_get_option('duo_roles', $all_roles); 
             $duo_auth = false;
+
+            /*
+             * Mainly a workaround for multisite login:
+             * if a user logs in to a site different from the one 
+             * they are a member of, login will work however
+             * it appears as if the user has no roles during authentication
+             * "fail closed" in this case and require duo auth
+             */
+            if(empty($user->roles)) {
+                $duo_auth = true;
+            }
 
             if (!empty($usr->roles) && is_array($usr->roles)) {
                 foreach ($usr->roles as $role) {
@@ -183,7 +194,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ?>
     <div class="wrap">
         <h2>Duo Two-Factor Authentication</h2>
-        <form action="options.php" method="post">
+        <?php if(is_multisite()) { ?>
+            <form action="ms-options.php" method="post">
+        <?php } else { ?>
+            <form action="options.php" method="post"> 
+        <?php } ?>
             <?php settings_fields('duo_settings'); ?>
             <?php do_settings_sections('duo_settings'); ?> 
             <p class="submit">
@@ -195,17 +210,17 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     }
 
     function duo_settings_ikey() {
-        $ikey = esc_attr(get_option('duo_ikey'));
+        $ikey = esc_attr(duo_get_option('duo_ikey'));
         echo "<input id='duo_ikey' name='duo_ikey' size='40' type='text' value='$ikey' />";
     }
 
     function duo_settings_skey() {
-        $skey = esc_attr(get_option('duo_skey'));
+        $skey = esc_attr(duo_get_option('duo_skey'));
         echo "<input id='duo_skey' name='duo_skey' size='40' type='text' value='$skey' />";
     }
 
     function duo_settings_host() {
-        $host = esc_attr(get_option('duo_host'));
+        $host = esc_attr(duo_get_option('duo_host'));
         echo "<input id='duo_host' name='duo_host' size='40' type='text' value='$host' />";
     }
 
@@ -218,7 +233,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
             $newroles[before_last_bar($key)] = before_last_bar($role);
         }
 
-        $selected = get_option('duo_roles', $newroles);
+        $selected = duo_get_option('duo_roles', $newroles);
 
         foreach ($wp_roles->get_names() as $role) {
             //create checkbox for each role
@@ -252,7 +267,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
         echo "<p>You can retrieve your integration key, secret key, and API hostname by logging in to the Duo administrative interface.</p>";
     }
 
-    function duo_ikey_validate($ikey){
+    function duo_ikey_validate($ikey) {
         if (strlen($ikey) != 20) {
             add_settings_error('duo_ikey', '', 'Integration key is not valid');
             return "";
@@ -272,19 +287,68 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
     function duo_admin_init() {
-        add_settings_section('duo_settings', 'Main Settings', 'duo_settings_text', 'duo_settings');
-        add_settings_field('duo_ikey', 'Integration key', 'duo_settings_ikey', 'duo_settings', 'duo_settings');
-        add_settings_field('duo_skey', 'Secret key', 'duo_settings_skey', 'duo_settings', 'duo_settings');
-        add_settings_field('duo_host', 'API hostname', 'duo_settings_host', 'duo_settings', 'duo_settings');
-        add_settings_field('duo_roles', 'Enable for roles:', 'duo_settings_roles', 'duo_settings', 'duo_settings');
-        register_setting('duo_settings', 'duo_ikey', 'duo_ikey_validate');
-        register_setting('duo_settings', 'duo_skey', 'duo_skey_validate');
-        register_setting('duo_settings', 'duo_host');
-        register_setting('duo_settings', 'duo_roles', 'duo_roles_validate');
+        if (is_multisite()) {
+            add_site_option('duo_ikey', '');
+            add_site_option('duo_skey', '');
+            add_site_option('duo_host', '');
+            add_site_option('duo_roles', '');
+        }
+        else {
+            add_settings_section('duo_settings', 'Main Settings', 'duo_settings_text', 'duo_settings');
+            add_settings_field('duo_ikey', 'Integration key', 'duo_settings_ikey', 'duo_settings', 'duo_settings');
+            add_settings_field('duo_skey', 'Secret key', 'duo_settings_skey', 'duo_settings', 'duo_settings');
+            add_settings_field('duo_host', 'API hostname', 'duo_settings_host', 'duo_settings', 'duo_settings');
+            add_settings_field('duo_roles', 'Enable for roles:', 'duo_settings_roles', 'duo_settings', 'duo_settings');
+            register_setting('duo_settings', 'duo_ikey', 'duo_ikey_validate');
+            register_setting('duo_settings', 'duo_skey', 'duo_skey_validate');
+            register_setting('duo_settings', 'duo_host');
+            register_setting('duo_settings', 'duo_roles', 'duo_roles_validate');
+        }
+
+    }
+
+    function duo_mu_options() {
+
+?>
+        <h3>Duo Security</h3>
+        <table class="form-table">
+            <?php duo_settings_text();?></td></tr>
+            <tr><th>Integration key</th><td><?php duo_settings_ikey();?></td></tr>
+            <tr><th>Secret key</th><td><?php duo_settings_skey();?></td></tr>
+            <tr><th>API hostname</th><td><?php duo_settings_host();?></td></tr>
+            <tr><th>Roles</th><td><?php duo_settings_roles();?></td></tr>
+        </table>
+<?php
+    }
+
+    function duo_update_mu_options() {
+        if(isset($_POST['duo_ikey'])) {
+            $ikey = $_POST['duo_ikey'];
+            $result = update_site_option('duo_ikey', $ikey);
+        }
+
+        if(isset($_POST['duo_skey'])) {
+            $skey = $_POST['duo_skey'];
+            $result = update_site_option('duo_skey', $skey);
+        }
+
+        if(isset($_POST['duo_host'])) {
+            $host = $_POST['duo_host'];
+            $result = update_site_option('duo_host', $host);
+        }
+
+        if(isset($_POST['duo_roles'])) {
+            $roles = $_POST['duo_roles'];
+            $result = update_site_option('duo_roles', $roles);
+        }
+
+
     }
 
     function duo_add_page() {
-        add_options_page('Duo Two-Factor', 'Duo Two-Factor', 'manage_options', 'duo_wordpress', 'duo_settings_page');
+        if(! is_multisite()) {
+            add_options_page('Duo Two-Factor', 'Duo Two-Factor', 'manage_options', 'duo_wordpress', 'duo_settings_page');
+        }
     }
 
     function duo_add_link($links, $file) {
@@ -305,7 +369,28 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
     add_filter('authenticate', 'duo_authenticate_user', 10, 3);
     add_filter('plugin_action_links', 'duo_add_link', 10, 2 );
-    add_action('admin_menu', 'duo_add_page');
+    if(is_multisite() && is_network_admin()){
+        add_action('network_admin_menu', 'duo_add_page');
+        
+        // Custom fields in network settings
+        add_filter('wpmu_options', 'duo_mu_options');
+        add_filter('update_wpmu_options', 'duo_update_mu_options');
+
+        
+
+    }
+    else {
+        add_action('admin_menu', 'duo_add_page');
+    }
     add_action('admin_init', 'duo_admin_init');
+
+    function duo_get_option($key, $default="") {
+        if (is_multisite()) {
+            return get_site_option($key, $default);
+        }
+        else {
+            return get_option($key, $default);
+        }
+    }
 
 ?>
