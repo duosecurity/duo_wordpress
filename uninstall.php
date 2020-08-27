35b41bebe7381cd002ba31cc67a2fa1c6a1552a5
vbscott <vbscott@umich.edu>
2020-08-27 11:18:27 -0400

    Remove unnecessary if statements (#18)
    Co-authored-by: Briar Scott <bscott@bscott-14399.local>

--- uninstall.php
+++ uninstall.php
@@ -4,22 +4,18 @@ defined('ABSPATH') or die('Direct Access Denied');
 //if uninstall not called from WordPress exit
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
         exit ();
-//if there are Duo credentials in wp_options, then delete all Duo credentials
-if (get_option('duo_ikey')) {
-    delete_option('duo_ikey');
-    delete_option('duo_skey');
-    delete_option('duo_host');
-    delete_option('duo_roles');
-    delete_option('duo_xmlrpc');
-}
+//Delete Duo credentials in wp_options
+delete_option('duo_ikey');
+delete_option('duo_skey');
+delete_option('duo_host');
+delete_option('duo_roles');
+delete_option('duo_xmlrpc');
 
-//if there are Duo credentials in wp_sitemeta, then delete all Duo credentials
-if (get_site_option('duo_ikey')) {
-    delete_site_option('duo_ikey');
-    delete_site_option('duo_skey');
-    delete_site_option('duo_host');
-    delete_site_option('duo_roles');
-    delete_site_option('duo_xmlrpc');
-}
+//Delete Duo credentials in wp_sitemeta
+delete_site_option('duo_ikey');
+delete_site_option('duo_skey');
+delete_site_option('duo_host');
+delete_site_option('duo_roles');
+delete_site_option('duo_xmlrpc');
 
 ?>
